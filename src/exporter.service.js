a63ae43e12123775b4472424fb92c8f7f522731c
Sergio Mendonça <sergio@mettzer.com>
2020-02-27 16:29:03 -0300

    filename

--- src/exporter.service.js
+++ src/exporter.service.js
@@ -1,12 +1,10 @@
 const XLSX = require('xlsx');
 
-const FILENAME = "statistics.xlsx";
-
 const exportStatsToXLSX = async (playersStats, nationalitiesStats) => {
   const wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(playersStats), "Players");
   XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(nationalitiesStats), "Nationalities");
-  return await XLSX.writeFile(wb, FILENAME, {
+  return await XLSX.writeFile(wb, "statistics.xlsx", {
     Props: {
       Author: "Sergio Mendonça"
     }
